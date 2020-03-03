d19e2b3da462ffdfa9d219d10e6333fbc8014bdf
lqqojin <lqqojin@gmail.com>
2020-03-03 19:46:00 +0900

    게임 완성

--- src/index.js
+++ src/index.js
@@ -2,23 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+/*
 class Square extends React.Component {
-	/**
-	 *  함수 컴포넌트
-	 *  1. 더 간단하게 컴포넌트 작성 방법
-	 *  2. state 없이 render 함수만을 가진다.
-	 *  3. React.Component를 확장하는데 클래스를 정의하는 대신
-	 *     Props를 입력받아서 렌더링 할 대상을 반환하는 함수를 작성할 수 있습니다.
-	 *  4. 클래스로 작성하는 것 보다 빠르게 작성할 수 있으며
-	 *     많은 컴포넌트를 함수 컴포넌트로 표현 할 수있다.
-	 *
-	 *  변경
-	 *  Square를 함수 컴포넌트로 수정했을 때
-	 *  onClick={() => this.props.onClick()}을
-	 *  onClick={props.onClick}로 간결하게 작성했습니다. 양
-	 *  쪽 모두 괄호가 사라진 것에 주목해주세요.
-	 */
-	render() {
+	render(props) {
 		return (
 			<button
 				className="square"
@@ -29,31 +15,67 @@ class Square extends React.Component {
 		);
 	}
 }
+*/
+
+/**
+*  함수 컴포넌트
+*  1. 더 간단하게 컴포넌트 작성 방법
+*  2. state 없이 render 함수만을 가진다.
+*  3. 'React.Component'를 확장하는데 클래스를 정의하는 대신
+*     'Props'를 입력받아서 렌더링 할 대상을 반환하는 함수를 작성할 수 있습니다.
+*  4. 클래스로 작성하는 것 보다 빠르게 작성할 수 있으며
+*     많은 컴포넌트를 함수 컴포넌트로 표현 할 수있다.
+*
+*  변경
+*  Square를 함수 컴포넌트로 수정했을 때
+*  onClick={() => this.props.onClick()}을
+*  onClick={props.onClick}로 간결하게 작성했습니다. 양
+*  쪽 모두 괄호가 사라진 것에 주목해주세요.
+*/
+const Square = (props) => {
+	return (
+		<button
+			className="square"
+			onClick={props.onClick}
+		>
+			{props.value}
+		</button>
+	)
+};
 
 class Board extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			squares: Array(9).fill(null),
+			xIsNext: true,
 		}
 	}
 	handleClick(i) {
-		console.log('this.state.squares', this.state.squares);
 		const squares = this.state.squares.slice();
-		console.log('this.state.squares.slice()', squares);
-		squares[i] = 'X';
-		this.setState({ squares });
+		if (calculateWinner(squares) || squares[i]) return;
+		squares[i] = this.state.xIsNext ? 'X' : 'O';
+		this.setState({
+			squares,
+			xIsNext: !this.state.xIsNext,
+		});
 	}
 	renderSquare(i) {
-		return <Square
+		return (<Square
 			value={this.state.squares[i]}
 			onClick={() => this.handleClick(i) }
-		/>;
+		/>);
 	}
 
 	render() {
-		const status = 'Next player: X';
-
+		// const status = 'Next player: X';
+		const winner = calculateWinner(this.state.squares);
+		let status;
+		if (winner) {
+			status = 'Winner: ' + winner;
+		} else {
+			status = `Next player: ${ (this.state.xIsNext ? 'X' : 'O') }`;
+		}
 		return (
 			<div>
 				<div className="status">{status}</div>
@@ -99,3 +121,24 @@ ReactDOM.render(
 	<Game />,
 	document.getElementById('root')
 );
+
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
+}
