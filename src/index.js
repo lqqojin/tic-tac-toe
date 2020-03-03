b2823defc2cedbb5b4e684a53f3d25e01db88852
lqqojin <lqqojin@gmail.com>
2020-03-03 15:04:48 +0900

    함수 컴포넌트 사용

--- src/index.js
+++ src/index.js
@@ -3,13 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+	/**
+	 *  함수 컴포넌트
+	 *  1. 더 간단하게 컴포넌트 작성 방법
+	 *  2. state 없이 render 함수만을 가진다.
+	 *  3. React.Component를 확장하는데 클래스를 정의하는 대신
+	 *     Props를 입력받아서 렌더링 할 대상을 반환하는 함수를 작성할 수 있습니다.
+	 *  4. 클래스로 작성하는 것 보다 빠르게 작성할 수 있으며
+	 *     많은 컴포넌트를 함수 컴포넌트로 표현 할 수있다.
+	 *
+	 *  변경
+	 *  Square를 함수 컴포넌트로 수정했을 때
+	 *  onClick={() => this.props.onClick()}을
+	 *  onClick={props.onClick}로 간결하게 작성했습니다. 양
+	 *  쪽 모두 괄호가 사라진 것에 주목해주세요.
+	 */
 	render() {
 		return (
 			<button
 				className="square"
-				onClick={ () => this.props.onClick() }
+				onClick={props.onClick}
 			>
-				{this.props.value}
+				{props.value}
 			</button>
 		);
 	}
